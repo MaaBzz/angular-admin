257573f0ea4798b03ed390b9d2267c75eb8a3567
gael bessac <gaelb57290@hotmail.fr>
2024-06-18 15:25:22 +0200

    add-retard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,8 @@ import { ManageUserComponent } from './manage-user/manage-user.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { LoginComponent } from './login/login.component';
 import { EditUserComponent } from './edit-user/edit-user.component';
+import { RetardComponent } from './retard/retard.component';
+import { AddRetardComponent } from './add-retard/add-retard.component';
 
 export const routes: Routes = [
   { path: 'accueil', component: HomeComponent },
@@ -11,6 +13,8 @@ export const routes: Routes = [
   { path: 'connexion', component: LoginComponent },
   { path: 'add-user', component: EditUserComponent },
   { path: 'edit-user/:id', component: EditUserComponent },
+  { path: 'retard', component: RetardComponent },
+  { path: 'add-retard/:id', component: AddRetardComponent },
   { path: '', redirectTo: 'accueil', pathMatch: 'full' },
   { path: '**', component: NotFoundComponent },
 ];
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,18 @@
 import { Component, inject } from '@angular/core';
-
+import { RouterLink } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { MatTableModule } from '@angular/material/table';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatIconModule } from '@angular/material/icon';
-import { RouterLink } from '@angular/router';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
-import { MatButtonModule } from '@angular/material/button';
-import { DatePipe } from '@angular/common';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss',
+})
 
 export class HomeComponent {
   
